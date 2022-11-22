f0fefb12942751b79d8f93bb4e6aa2bbf899db1c
Bhavdeep Singh <gorayabhavdeep@gmail.com>
2022-11-22 15:38:40 -0800

    Working with multiple event listeners

--- scripts/script.js
+++ scripts/script.js
@@ -5,5 +5,32 @@ function clickedHandler(){
     alert("You clicked Me!")
     // clickButton.removeEventListener('click', clickedHandler)
 }
-clickButton.addEventListener('click', clickedHandler, {once:true});
+// clickButton.addEventListener('click', clickedHandler, {once:true});
 
+// function buttonDarkTheme(e){
+//     clickButton.classList.add('click-meDarktheme')
+// };
+// clickButton.addEventListener('click', buttonDarkTheme);
+// const textcontent = "Clicked!" 
+function changeBGpink(){
+    document.body.classList.toggle("pink")
+};
+
+function changeText(){
+    if ( clickButton.textContent === "Click Me!"){
+        clickButton.textContent = "Clicked!"
+    }
+    else{
+        clickButton.textContent = "Click Me!"
+    }
+};
+
+clickButton.addEventListener("click",changeBGpink);
+
+clickButton.addEventListener("click",changeText);
+
+
+// const image = document.querySelector('#shoppingCart')
+// function Addimage(e){
+    
+// }
