22f89fd9292bd8c0ec7a11fec3c5531387db9ac3
Bhavdeep Singh <gorayabhavdeep@gmail.com>
2022-11-22 14:55:14 -0800

    Working with removeEventListener

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const clickButton = document.querySelector('.click-me');
 // console.log(clickButton)
 function clickedHandler(){
     alert("You clicked Me!")
+    // clickButton.removeEventListener('click', clickedHandler)
 }
-clickButton.addEventListener('click', clickedHandler);
+clickButton.addEventListener('click', clickedHandler, {once:true});
 
